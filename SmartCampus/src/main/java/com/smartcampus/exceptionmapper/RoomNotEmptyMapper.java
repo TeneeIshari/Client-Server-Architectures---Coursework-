93681a21cf82640df2108f5ed759dc801f35dd36
TeneeIshari <teneesha.alwis@gmail.com>
2026-04-03 10:50:24 +0530

    Created exception mapper file for empty room.

--- SmartCampus/src/main/java/com/smartcampus/exceptionmapper/RoomNotEmptyMapper.java
+++ SmartCampus/src/main/java/com/smartcampus/exceptionmapper/RoomNotEmptyMapper.java
@@ -4,10 +4,19 @@
  */
 package com.smartcampus.exceptionmapper;
 
-/**
- *
- * @author indika
- */
-public class RoomNotEmptyMapper {
-    
+import com.smartcampus.exception.RoomNotEmptyException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RoomNotEmptyMapper implements ExceptionMapper<RoomNotEmptyException> {
+
+    @Override
+    public Response toResponse(RoomNotEmptyException ex) {
+        return Response.status(Response.Status.CONFLICT) // 409
+                .entity("{\"error\":\"" + ex.getMessage() + "\"}")
+                .build();
+    }
 }

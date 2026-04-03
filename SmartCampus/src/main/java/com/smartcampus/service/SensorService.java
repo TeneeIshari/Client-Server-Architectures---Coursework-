1ee3831eddf1f47ed11200104b3184933948d220
TeneeIshari <teneesha.alwis@gmail.com>
2026-04-03 10:40:16 +0530

    Created Sensor Service

--- SmartCampus/src/main/java/com/smartcampus/service/SensorService.java
+++ SmartCampus/src/main/java/com/smartcampus/service/SensorService.java
@@ -4,10 +4,47 @@
  */
 package com.smartcampus.service;
 
+import com.smartcampus.exception.LinkedResourceNotFoundException;
+import com.smartcampus.model.Room;
+import com.smartcampus.model.Sensor;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author indika
  */
 public class SensorService {
+    
+    private final Map<String, Sensor> sensors = new Hashmap<>();
+    private final RoomService roomService = new RoomService();
+    
+    public Collection<Sensor> getAllSensors(){
+        return sensors.values();
+    }
+    
+    public Collection<Sensor> getSensorsByType(String type){
+        return sensors.values().stream().filter(sensor -> sensor.getType().equalsIgnoreCase(type)).collect(Collectors.toList());
+    }
+    
+    public Sensor getSeonsor(String id){
+        return sensors.get(id);
+    }
+    
+    // Create a new sensor
+    public Sensor createSensor(Sensor sensor){
+        Room room  = roomService.getRoom(sensor.getRoomId());
+        
+        if(room == null){
+            throw new LinkedResourceNotFoundException("Room with ID " + sensor.getRoomId() + " does not exist");
+        }
+        
+        sensors.put(sensor.getId(), sensor);
+        
+        room.getSensorsId().add(sensor.getId());
+        
+        return sensor;
+    }
     
 }

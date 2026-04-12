8b4f4b0501d147a7c079b298cfa3f6cecafe5dfe
TeneeIshari <teneesha.alwis@gmail.com>
2026-04-12 12:11:04 +0530

    Created DiscoveryResource file.

--- SmartCampus/src/main/java/com/smartcampus/resource/DiscoveryResource.java
+++ SmartCampus/src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -4,10 +4,41 @@
  */
 package com.smartcampus.resource;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
- * @author indika
+ * @author Teneesha
  */
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
 public class DiscoveryResource {
+    
+    @GET
+    public Map<String, Object> getApiInfo(){
+        Map<String, Object> response = new HashMap<>();
+        
+        // API Metadatada
+        response.put("api", "Smart Campus API");
+        response.put("version", "v1");
+        response.put("api", "Smart Campus API");
+        
+        // Resource links
+        Map<String, String> resources = new HashMap<>();
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+        
+        response.put("resources", resources);
+        
+        return response;
+        
+        
+    }
     
 }

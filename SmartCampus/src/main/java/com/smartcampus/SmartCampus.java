72d2884c15a47e5c1b0c1cfcadcacd526d1f2267
TeneeIshari <teneesha.alwis@gmail.com>
2026-04-17 19:36:16 +0530

    Created the Main file.

--- SmartCampus/src/main/java/com/smartcampus/SmartCampus.java
+++ SmartCampus/src/main/java/com/smartcampus/SmartCampus.java
@@ -4,13 +4,29 @@
 
 package com.smartcampus;
 
+import java.net.URI;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
 /**
  *
- * @author indika
+ * @author Teneesha
  */
 public class SmartCampus {
 
+    public static final String BASE_URI = "http://localhost:8080/api/v1/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("com.smartcampus");
+
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final HttpServer server = startServer();
+        System.out.println("Server started at " + BASE_URI);
+        
     }
 }
+
